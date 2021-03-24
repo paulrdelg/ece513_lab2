f723a7555b395f646c2fd74c306e9d603864c112
Paul Delgado <paulrdelgado@gmail.com>
2021-03-23 21:45:18 -0700

    read image

--- main.py
+++ main.py
@@ -4,63 +4,24 @@ import platform
 import sys
 
 # Common Third-Party Packages
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 from sklearn.decomposition import PCA
 
 # Custom
-import lab1
+import lab2
 
 def main():
-	# Load matrix of 1D column vectors (X)
-	X = np.load('mydata/X.npy')
-	print('X:   ', X.shape)
+	# Read image file
+	filepath = 'pepper_copy.PNG'
+	title = 'Copy of Pepper Image (Uncompressed)'
 	
-	# Retrieve Dimensions
-	N = X.shape[0]
-	M = X.shape[1]
-	print('N:   ', N)
-	print('M:   ', M)
-	
-	# Load SVD
-	U = np.load('mydata/U.npy')
-	Sigma = np.load('mydata/Sigma.npy')
-	VT = np.load('mydata/VT.npy')
-	print('U:   ', U.shape)
-	print('S:   ', Sigma.shape)
-	print('VT:  ', VT.shape)
-	
-	# Determine mean image (m)
-	m = np.load('mydata/m.npy')
-	print('m:   ', m.shape)
-	
-	# compute mean centered matrix (W)
-	W = np.load('mydata/W.npy')
-	WT = W.transpose()
-	print('W:   ', W.shape)
-	print('WT:  ', WT.shape)
-	
-	# Compute Covariance (C)
-	C = np.load('mydata/C.npy')
-	
-	# Eigenvalues & Eigenvectors
-	fp = 'mydata/eigenvalues.npy'
-	eigenvalues = np.load(fp)
-	pca = np.load('mydata/pca.npy')
-	print(pca)
-	print(pca.shape)
-	
-	# Reconstruct with 4th pca
-	a = np.zeros(X.shape[0])
-	print(a.shape)
-	count = 0
-	for i in (0, 3):
-		w = W[i]
-		a = a + np.matmul(W[i], VT[i])
-		count = count + 1
-	a = a.reshape((304, 200))
-	lab1.showImage(a)
+	# Read image
+	#img = cv2.imread(filepath)
+	#img = mpl.image.imread(filepath)
+	img = PIL.Image.open(filepath)
 	
 	return 0
 
